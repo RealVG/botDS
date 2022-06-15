2ca9f7452f306d174dbd9fb1e11752540245beb4
RealVG <107572070+RealVG@users.noreply.github.com>
2022-06-15 19:37:39 +0200

    new var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     {intents:['GUILDS','GUILD_MEMBERS','GUILD_BANS','GUILD_EMOJIS_AND_STICKERS','GUILD_INTEGRATIONS','GUILD_WEBHOOKS','GUILD_INVITES','GUILD_VOICE_STATES','GUILD_PRESENCES','GUILD_MESSAGES','GUILD_MESSAGE_REACTIONS','GUILD_MESSAGE_TYPING','DIRECT_MESSAGES','DIRECT_MESSAGE_REACTIONS','DIRECT_MESSAGE_TYPING','GUILD_SCHEDULED_EVENTS']}    
 )
 
-client.login('process.env.token')
+client.login(process.env.token)
 client.on("ready",() => {
     console.log('IMMA READY!');
 })
