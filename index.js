20bbefea02268f79143d9419e0992d0e44e2e8fb
RealVG <107572070+RealVG@users.noreply.github.com>
2022-06-16 10:05:17 +0200

    tickets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
-const client = new Discord.Client(
-    {intents:['GUILDS','GUILD_MEMBERS','GUILD_BANS','GUILD_EMOJIS_AND_STICKERS','GUILD_INTEGRATIONS','GUILD_WEBHOOKS','GUILD_INVITES','GUILD_VOICE_STATES','GUILD_PRESENCES','GUILD_MESSAGES','GUILD_MESSAGE_REACTIONS','GUILD_MESSAGE_TYPING','DIRECT_MESSAGES','DIRECT_MESSAGE_REACTIONS','DIRECT_MESSAGE_TYPING','GUILD_SCHEDULED_EVENTS']}    
+const client = new Discord.Client( 
+    {partials:['MESSAGE','CHANNEL','REACTION'],intents:['GUILDS','GUILD_MEMBERS','GUILD_BANS','GUILD_EMOJIS_AND_STICKERS','GUILD_INTEGRATIONS','GUILD_WEBHOOKS','GUILD_INVITES','GUILD_VOICE_STATES','GUILD_PRESENCES','GUILD_MESSAGES','GUILD_MESSAGE_REACTIONS','GUILD_MESSAGE_TYPING','DIRECT_MESSAGES','DIRECT_MESSAGE_REACTIONS','DIRECT_MESSAGE_TYPING','GUILD_SCHEDULED_EVENTS']} 
 )
 
 client.login(process.env.token)
@@ -36,13 +36,47 @@ client.on("messageCreate",(message) =>{
 
 //tickets
 
-
+//messaggio per inizializzare i ticket
 client.on('message', message=>{
      if(message.content=="!startticket"){
          message.channel.send('clicca sulla reazione per aprire un ticket')
          .then(msg=>msg.react('✉️'))
      }
- 
- 
- 
- })
+})
+
+//sistema ticket
+
+client.on("messageReactionAdd",async function(messageReaction,user){
+
+     if(user.bot) return
+
+     if(messageReaction.message.partial)await messageReaction.message.fetch();
+
+     if(messageReaction._emoji.name == "✉️"){
+          if(messageReaction.message.channel.id =='986894236513349642')
+          messageReaction.users.remove();
+          var server = messageReaction.message.channel.guild;
+          if(server.channel.cache.find(canale => canale.topic ==`User ID: ${user.id}`)){
+               user.send("hai gia un ticket aperto!").catch(()=>{ })
+          }
+          server.channel.create(user.username,{
+               type:"text"
+          }).then(canale =>{
+               canale.setTopic(`User ID: ${user.id}`);
+               canale.setParent("986902625842774017")
+               canale.overwritePermissions([
+                    {
+                         id:server.id,
+                         deny:["VIEW_CHANNEL"]
+                    },
+                    {
+                         id:user.id,
+                         allow:["VIEW_CHANNEL"]
+                    }
+               ])
+               canale.send('ecco il tuo ticket, spiega il tuo problema')
+          })
+     }
+
+
+})
